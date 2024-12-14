286135bfd689bfece38490b3676a205f8e8dd70f
RoyHridoy <royhridoy1428@gmail.com>
2024-12-14 20:34:50 +0600

    working: send scheduled email notification to the user

---
+++ app/Notifications/VaccineDateScheduled.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class VaccineDateScheduled extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    /**
+     * Create a new notification instance.
+     */
+    public function __construct(private string $vaccineCenterName, private string $date)
+    {
+        //
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @return array<int, string>
+     */
+    public function via(object $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(object $notifiable): MailMessage
+    {
+        return (new MailMessage())
+            ->subject('Schedule for Vaccination')
+            ->line("Your vaccination appointment at {$this->vaccineCenterName} is scheduled for {$this->date}.")
+            ->line("Please bring your NID card.")
+            ->line('Thanks!');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,9 @@
 use App\Enum\VaccineStatus;
 use App\Models\User;
 use App\Models\VaccineCenter;
+use App\Notifications\VaccineDateScheduled;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'welcome');
@@ -14,14 +16,14 @@ Route::get('/test', function () {
     // dump(VaccineCenter::find(3)->users()->where('users.status', 1)->pluck('id'));
     // dump(VaccineCenter::find(4)->users()->where('users.status', 1)->pluck('id'));
     // dump(VaccineCenter::find(5)->users()->where('users.status', 1)->pluck('id'));
-    dump(User::where('status', 1)->pluck('id'));
+    dump('Remains for vaccination', User::where('status', 1)->pluck('id'));
 
     // 1. Mark users as vaccinated
     $vaccinatedUsers = User::where('status', VaccineStatus::SCHEDULED->value)->pluck('id');
     DB::table('users')->whereIn('id', $vaccinatedUsers)->update([
         'status' => VaccineStatus::VACCINATED->value,
     ]);
-    dump($vaccinatedUsers);
+    dump('today vaccinated users ', $vaccinatedUsers);
 
     // 2. Schedule for the next users
     // 2.1 Load only specific user but N+1 query problem. where N is the number of vaccine_center
@@ -35,7 +37,7 @@ Route::get('/test', function () {
                 ->limit($center->daily_seat_limit)
                 ->pluck('id');
         });
-    dump($userIdsToBeScheduled); // Number of users will be vaccinated tomorrow
+    dump('Number of user will be vaccinated tomorrow', $userIdsToBeScheduled); // Number of users will be vaccinated tomorrow
 
     // 2.1 (alternative) Eager loading but loads all the users
     // $vaccineCenters = VaccineCenter::pluck('daily_seat_limit', 'id');
@@ -54,6 +56,16 @@ Route::get('/test', function () {
         'status' => VaccineStatus::SCHEDULED->value,
         'scheduled_at' => today()->addDay(),
     ]);
+
+    // 3 Send Notification to the scheduled users
+    User::whereIn('id', $userIdsToBeScheduled)->with('vaccineCenter:id,name')->chunk(300, function ($users) {
+        foreach ($users as $user) {
+            Notification::send($user, new VaccineDateScheduled(
+                $user->vaccineCenter->name,
+                today()->addDay()->toFormattedDayDateString()
+            ));
+        }
+    });
 });
 
 Route::view('dashboard', 'dashboard', [
