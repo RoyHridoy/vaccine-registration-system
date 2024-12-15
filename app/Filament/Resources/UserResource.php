973c42843a9c5fa61bb2c6a032a4aecd98b0ddc2
RoyHridoy <royhridoy1428@gmail.com>
2024-12-15 13:06:11 +0600

    feature: Show users lists with filter based on status and vaccine center in admin panel

---
+++ app/Filament/Resources/UserResource.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Enum\VaccineStatus;
+use App\Filament\Resources\UserResource\Pages;
+use App\Filament\Resources\UserResource\RelationManagers;
+use App\Models\User;
+use App\Models\VaccineCenter;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Filters\Filter;
+use Filament\Tables\Filters\SelectFilter;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class UserResource extends Resource
+{
+    protected static ?string $model = User::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-users';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                //
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('name'),
+                TextColumn::make('nid')->label('NID')->icon('heroicon-o-identification'),
+                TextColumn::make('email')->label('Email Address'),
+                TextColumn::make('vaccineCenter.name'),
+                TextColumn::make('status')
+                    ->label('Vaccine Status')
+                    ->formatStateUsing(fn ($state) => VaccineStatus::tryFrom($state->value)->name)
+                    ->badge()
+                    ->color(fn ($state): string => match ($state->name) {
+                        'NOT_SCHEDULED' => 'gray',
+                        'SCHEDULED' => 'warning',
+                        'VACCINATED' => 'success',
+                    }),
+                TextColumn::make('scheduled_at')
+                    ->label('Date')
+                    ->date()
+                    ->alignEnd(),
+            ])
+            ->filters([
+                SelectFilter::make('status')
+                    ->multiple()
+                    ->options([
+                        1 => 'Not Scheduled',
+                        2 => 'Scheduled',
+                        3 => 'Vaccinated',
+                    ]),
+                SelectFilter::make('vaccine_center_id')
+                    ->multiple()
+                    ->label('Vaccine Center')
+                    ->options(
+                        VaccineCenter::pluck('name', 'id')->toArray()
+                    )
+            ])
+            ->actions([
+
+            ])
+            ->bulkActions([
+
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListUsers::route('/'),
+        ];
+    }
+}
